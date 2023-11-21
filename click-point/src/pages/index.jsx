630ca2502efd599da82091e6f121a987ebfdb87a
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-11-21 14:09:00 -0300

    Feature: Initial commit

---
+++ click-point/src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='bg-black w-full min-h-screen'>
+      <header></header>
+      <section>
+        <h1 className='text-white'>Olá mundo!</h1>
+      </section>
+    </main>
+  )
+}
---
+++ click-point/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{jsx,ts,js,tsx,mdx}',
+    './src/components/**/*.{jsx,ts,js,tsx,mdx}',
+    './src/app/**/*.{jsx,ts,js,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+      },
+    },
+  },
+}
